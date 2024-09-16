bad280a88f8bc3e1465500cb75cb1ce7c750ed2e
Jaro1977 <132671443+Jaro1977@users.noreply.github.com>
2024-09-16 14:41:07 +0200

    Add files via upload

---
+++ 07_Multilabel Classification/architecture/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 12:57:02 2024
+
+@author: jarog
+"""
+
---
+++ 07_Multilabel Classification/architecture/models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 12:56:29 2024
+
+@author: jarog
+"""
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+
+
+class VGGNetSmall:
+
+    def __init__(self, input_shape, num_classes, final_activation):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.final_activation = final_activation
+
+    def build(self):
+        model = Sequential()
+
+        model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=self.input_shape, activation='relu'))
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(units=1024, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(units=self.num_classes, activation=self.final_activation))
+
+        return model
